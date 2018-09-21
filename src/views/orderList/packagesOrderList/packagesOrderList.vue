3c2aedaade937b5894885ed02d19128d7d0014a5
zhouyunyao <1054603363@qq.com>
2018-09-21 15:35:09 +0800

    修改bug

--- src/views/orderList/packagesOrderList/packagesOrderList.vue
+++ src/views/orderList/packagesOrderList/packagesOrderList.vue
@@ -18,9 +18,7 @@
             type="daterange"
             range-separator="至"
             start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            format="yyyy 年 MM 月 dd 日"
-            value-format="yyyy-MM-dd">
+            end-placeholder="结束日期">
           </el-date-picker>
         </el-form-item>
         <el-form-item>
